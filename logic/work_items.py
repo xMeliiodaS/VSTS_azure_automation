3633671a2e7d776e18c27ec0c7ff7252369f4783
bahaa.abozlf57@gmail.com <yourgithub@email.com>
2025-07-15 21:31:09 +0300

    added init to work_items

--- logic/work_items.py
+++ logic/work_items.py
@@ -3,4 +3,19 @@ from selenium.webdriver.common import by
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-class OpenAzureAstsTest:
+from infra.base_page import BasePage
+class OpenAzureAstsTest(BasePage):
+    # Locators
+
+    def __init__(self, driver):
+        """
+        Initializes the BoardPage with the provided WebDriver instance.
+        :param driver: The WebDriver instance to use for browser interactions.
+        """
+        super().__init__(driver)
+
+    def click_on_search(self):
+        """
+        Clicks the search bar.
+        """
+
