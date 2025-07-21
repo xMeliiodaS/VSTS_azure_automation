76ed460f0887b6e2301f19dadd2d0e32bb4fe3b3
Abozalaf <babozala@its.jnj.com>
2025-07-21 13:10:56 +0300

    Modified the Search bug and Click on result

--- logic/work_items_search.py
+++ logic/work_items_search.py
@@ -1,3 +1,4 @@
+from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -8,7 +9,7 @@ from infra.base_page import BasePage
 class WorkItemsSearch(BasePage):
     # -----------------Locators Related to Bugs-----------------
     SEARCH_BAR_INPUT = '#__bolt-textfield-input-1'
-    BUG_OBJECT = 'div[data-automationid="DetailsRow"][data-item-key="0"]'
+    BUG_OBJECT = 'a.work-item-title-link'
 
     def __init__(self, driver):
         """
@@ -19,10 +20,16 @@ class WorkItemsSearch(BasePage):
 
     def fill_bug_id_input(self, bug_id):
         """
-        Fills the Bug ID on the Search Bar.
+        Clear Search bar and fills the Bug ID on the Search Bar.
         """
-        WebDriverWait(self._driver, 20).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, self.SEARCH_BAR_INPUT))).send_keys(bug_id)
+        search_bar = WebDriverWait(self._driver, 20).until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, self.SEARCH_BAR_INPUT)))
+
+        search_bar.click()
+        search_bar.send_keys(Keys.CONTROL + "a")
+        search_bar.send_keys(Keys.DELETE)
+
+        search_bar.send_keys(bug_id)
 
     def click_on_searched_bug_row(self):
         """
@@ -30,3 +37,5 @@ class WorkItemsSearch(BasePage):
         """
         WebDriverWait(self._driver, 20).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, self.BUG_OBJECT))).click()
+
+
