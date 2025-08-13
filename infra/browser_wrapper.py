55cade170dc7ed80dfa3feaaffa4436aac686697
Abozalaf <babozala@its.jnj.com>
2025-08-13 10:14:14 +0300

    Check Point

--- infra/browser_wrapper.py
+++ infra/browser_wrapper.py
@@ -32,7 +32,7 @@ class BrowserWrapper:
         try:
             options = webdriver.ChromeOptions()
 
-            # options.add_argument("--headless")
+            options.add_argument("--headless")
             options.add_argument("--disable-gpu")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
