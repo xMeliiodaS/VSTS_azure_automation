3ca6690ca0e718d8c5cef1ee8e9116572158f91a
Abozalaf <babozala@its.jnj.com>
2025-08-14 16:32:55 +0300

    Added auto open to HTML file

--- utils/html_reporter.py
+++ utils/html_reporter.py
@@ -1,62 +1,62 @@
 # logic/html_reporter.py
 
 import pandas as pd
-
+import os
 
 def export_html_report(results, report_title="📝 Automation Results for STD_ID Validation",
                        filename='std_id_check_results.html'):
     TABLE_STYLE = '''
-<style>
-  body {
-    font-family: 'Segoe UI', Arial, sans-serif;
-    background: #262a34;
-    color: #f1f1fa;
-  }
-  table {
-    border-collapse: collapse;
-    width: 80%;
-    margin: 24px auto;
-    background: #32364a;
-    box-shadow: 0 2px 16px #22263a44;
-    border-radius: 10px;
-    overflow: hidden;
-  }
-  th, td {
-    padding: 12px 15px;
-    border: 1px solid #424758;
-    text-align: left;
-  }
-  th {
-    background: linear-gradient(90deg, #4e59c2 0%, #9755e4 100%);
-    color: #fff;
-    font-weight: 600;
-    letter-spacing: .02em;
-    border: none;
-  }
-  tr {
-    background: #32364a;
-  }
-  tr:nth-child(even) {
-    background: #373d52;
-  }
-  tr:hover {
-    background: #44476b;
-    transition: background 0.12s;
-  }
-  td:last-child {
-    font-style: italic;
-    color: #bab8ea;
-  }
-  .pass {
-    color: #3ff7b6;
-    font-weight: bold;
-  }
-  .fail {
-    color: #ff6584;
-    font-weight: bold;
-  }
-</style>
-    '''
+        <style>
+          body {
+            font-family: 'Segoe UI', Arial, sans-serif;
+            background: #262a34;
+            color: #f1f1fa;
+          }
+          table {
+            border-collapse: collapse;
+            width: 80%;
+            margin: 24px auto;
+            background: #32364a;
+            box-shadow: 0 2px 16px #22263a44;
+            border-radius: 10px;
+            overflow: hidden;
+          }
+          th, td {
+            padding: 12px 15px;
+            border: 1px solid #424758;
+            text-align: left;
+          }
+          th {
+            background: linear-gradient(90deg, #4e59c2 0%, #9755e4 100%);
+            color: #fff;
+            font-weight: 600;
+            letter-spacing: .02em;
+            border: none;
+          }
+          tr {
+            background: #32364a;
+          }
+          tr:nth-child(even) {
+            background: #373d52;
+          }
+          tr:hover {
+            background: #44476b;
+            transition: background 0.12s;
+          }
+          td:last-child {
+            font-style: italic;
+            color: #bab8ea;
+          }
+          .pass {
+            color: #3ff7b6;
+            font-weight: bold;
+          }
+          .fail {
+            color: #ff6584;
+            font-weight: bold;
+          }
+        </style>
+            '''
 
     df = pd.DataFrame(results)
 
@@ -78,5 +78,5 @@ def export_html_report(results, report_title="📝 Automation Results for STD_ID
 
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(html_output)
-
+        os.startfile(filename)
     print(f"✅ Fancy HTML report generated: {filename}")
